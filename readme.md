b8719c8f68b145b7547073872759be8d1c2e41a2
QiubyZ <lessonphreaker@gmail.com>
2024-11-15 02:20:42 +0800

    first commit

--- readme.md
+++ readme.md
@@ -1,5 +1,28 @@
-# Acode plugin
+# JDTLS Java Language Server
 
-> For typescript version of plugin template switch this repo to main-ts
+This Plugin for Java Language Server with support **Args** and **Dynamic Executable file Jdtls**.
 
-Read acode plugin [documentation](https://acode.foxdebug.com/plugin-docs) to develop plugin for acode editor.
+The default configuration is taken from Nvim **customization**
+
+with:
+  - **Executable File Path**: _/data/data/com.termux/files/home/.local/share/nvim/mason/bin/jdtls_
+  - **Arguments**: _-configuration /data/data/com.termux/files/home/.cache/jdtls/config -data /data/data/com.termux/files/home/.cache/jdtls/workspace_
+
+Change it if you want to change it.
+
+## Setup
+  1. If you don't have **JDTLS**, you can download on [jdt-language-server-1.41.tar.gz](https://www.eclipse.org/downloads/download.php?file=/jdtls/milestones/1.41.0/jdt-language-server-1.41.0-202410311350.tar.gz)
+  2. Extract the file on your Termux
+  3. Open the Folder 
+  4. Search executable file in `bin/jdtls`
+  5. Copy FullDirectoryPath of `jdtls` executable 
+  6. And paste on Plugin **JDTLS Java Language Server** Settings
+  7. Remove Default Text of Argument or edit your settings.json to `args: []`
+     
+      example:
+        ```json
+      "acode.plugin.jdts": {
+        "serverPath": "your/path/bin/jdtls",
+        "args": []
+      }
+        ```
--- src/main.js
+++ src/main.js
@@ -1,14 +1,104 @@
 import plugin from '../plugin.json';
+let AppSettings = acode.require("settings")
 
 class AcodePlugin {
 
   async init() {
+    // your plugin codes goes here
+    let acodeLanguageClient = acode.require("acode-language-client");
+
+    if (acodeLanguageClient) {
+      await this.setupLanguageClient(acodeLanguageClient);
+    } else {
+      window.addEventListener("plugin.install", ({ detail }) => {
+        if (detail.name === "acode-language-client") {
+          acodeLanguageClient = acode.require("acode-language-client");
+          this.setupLanguageClient(acodeLanguageClient);
+        }
+      });
+    }
 
   }
 
+  get defaultSettings() {
+    return {
+      serverPath: "/data/data/com.termux/files/home/.local/share/nvim/mason/bin/jdtls",
+      args: [
+        "-configuration /data/data/com.termux/files/home/.cache/jdtls/config",
+        "-data /data/data/com.termux/files/home/.cache/jdtls/workspace"],
+    }
+  }
+  async setupLanguageClient(acodeLanguageClient) {
+    let JDTLSsocket = acodeLanguageClient.getSocketForCommand(
+      this.settings.serverPath,
+      this.settings.args,
+    );
+    let JDTLSClient = new acodeLanguageClient.LanguageClient({
+      type: "socket",
+      socket: JDTLSsocket,
+    });
+
+    acodeLanguageClient.registerService("java", JDTLSClient);
+
+    acode.registerFormatter(plugin.name, ["java"], () =>
+      acodeLanguageClient.format(),
+    );
+
+  }
+  get settings() {
+    // UPDATE SETTING SAAT RESTART ACODE
+    if (!window.acode) return this.defaultSettings;
+    let value = AppSettings.value[plugin.id];
+    if (!value) {
+      //Menjadikan Method defaultSettings sebagai nilai Default
+      value = AppSettings.value[plugin.id] = this.defaultSettings;
+      AppSettings.update();
+    }
+    return value;
+  }
+  get settingsMenuLayout() {
+    return {
+      list: [
+        {
+          index: 0,
+          key: "serverPath",
+          promptType: "text",
+          info: "Set the JDTLS executable file Path",
+          prompt: "Set the JDTLS executable file Path",
+          text: "JDTLS Executable File Path",
+          value: this.settings.serverPath,
+        },
+        {
+          index: 1,
+          key: "args",
+          promptType: "text",
+          info: "JDTLS Argument",
+          prompt: "JDTLS Args",
+          text: "JDTLS Argument",
+          value: this.settings.args.join(", ")
+        },
+      ],
+      cb: (key, value) => {
+
+        switch (key) {
+          case 'args':
+            value = value ? value.split(",").map(item => item.trim()) : [];
+            break;
+        }
+        AppSettings.value[plugin.id][key] = value;
+        AppSettings.update();
+      },
+
+    };
+  }
   async destroy() {
-
+    // Add your cleanup code here
+    if (AppSettings.value[plugin.id]) {
+      delete AppSettings.value[plugin.id];
+      AppSettings.update();
+    }
   }
+
 }
 
 if (window.acode) {
@@ -19,7 +109,8 @@ if (window.acode) {
     }
     acodePlugin.baseUrl = baseUrl;
     await acodePlugin.init($page, cacheFile, cacheFileUrl);
-  });
+  }, acodePlugin.settingsMenuLayout);
+
   acode.setPluginUnmount(plugin.id, () => {
     acodePlugin.destroy();
   });
