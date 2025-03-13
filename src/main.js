fc33a26ff8315f70e68cd6dc35fb6e414a1ed403
QiubyZ <lessonphreaker@gmail.com>
2025-03-13 22:03:23 +0800

    Add Formatter Plugin

--- src/main.js
+++ src/main.js
@@ -2,149 +2,210 @@ import plugin from "../plugin.json";
 let AppSettings = acode.require("settings");
 
 class AcodePlugin {
-  constructor() {
-    this.name_language_type = "java"
-    this.languageserver = "/data/data/com.termux/files/home/LANGUAGE-SERVER/jdtls/jdtls/bin/jdtls"
-    this.standart_args = []
-    this.initializeOptions = {
-      initializationOptions: {}
+    constructor() {
+        this.name_language_type = "java";
+        this.languageserver = "jdtls";
+        this.standart_args = [];
+        this.initializeOptions = {
+            initializationOptions: {},
+        };
     }
-  }
 
-  async init($page, cacheFile, cacheFileUrl) {
-    let acodeLanguageClient = acode.require("acode-language-client");
-    if (acodeLanguageClient) {
-      await this.setupLanguageClient(acodeLanguageClient);
-    } else {
-      window.addEventListener("plugin.install", ({ detail }) => {
-        if (detail.name === "acode-language-client") {
-          acodeLanguageClient = acode.require("acode-language-client");
-          this.setupLanguageClient(acodeLanguageClient);
+    async init($page, cacheFile, cacheFileUrl) {
+        let acodeLanguageClient = acode.require("acode-language-client");
+        if (acodeLanguageClient) {
+            await this.setupLanguageClient(acodeLanguageClient);
+        } else {
+            window.addEventListener("plugin.install", ({ detail }) => {
+                if (detail.name === "acode-language-client") {
+                    acodeLanguageClient = acode.require(
+                        "acode-language-client",
+                    );
+                    this.setupLanguageClient(acodeLanguageClient);
+                }
+            });
         }
-      });
     }
-  }
-  
-  get settings() {
-    // UPDATE SETTING SAAT RESTART ACODE
-    if (!window.acode) return this.defaultSettings;
-    let value = AppSettings.value[plugin.id];
-    if (!value) {
-      //Menjadikan Method defaultSettings sebagai nilai Default
-      value = AppSettings.value[plugin.id] = this.defaultSettings;
-      AppSettings.update();
-    }
-    return value;
-  }
-  
-  get settingsMenuLayout() {
-    return {
-      list: [
-        {
-          index: 0,
-          key: "serverPath",
-          promptType: "text",
-          prompt: "Change the serverPath before running.",
-          text: "JDTLS Executable File Path",
-          value: this.settings.serverPath,
-        },
-        {
-          index: 1,
-          key: "arguments",
-          promptType: "text",
-          info: "For multiple arguments, please use comma ','<br>Example: --stdio, -v, -vv",
-          prompt: "Argument Of Language Server",
-          text: "JDTLS Argument",
-          value: this.settings.arguments.join(", ")
-        },
-      ],
 
-      cb: (key, value) => {
-        switch (key) {
-          case 'arguments':
-            value = value ? value.split(",").map(item => item.trim()) : [];
-            break;
+    get settings() {
+        // UPDATE SETTING SAAT RESTART ACODE
+        if (!window.acode) return this.defaultSettings;
+        let value = AppSettings.value[plugin.id];
+        if (!value) {
+            //Menjadikan Method defaultSettings sebagai nilai Default
+            value = AppSettings.value[plugin.id] = this.defaultSettings;
+            AppSettings.update();
         }
-        AppSettings.value[plugin.id][key] = value;
-        AppSettings.update();
-      },
-    };
-  }
-
-  get defaultSettings() {
-    return {
-      serverPath: this.languageserver,
-      arguments: this.standart_args,
-      languageClientConfig: this.initializeOptions
-    };
-  }
-
-  async setupLanguageClient(acodeLanguageClient) {
-    let socket = acodeLanguageClient.getSocketForCommand(
-      this.settings.serverPath,
-      this.settings.arguments,
-    );
-
-
-    let JavaClient = new acodeLanguageClient.LanguageClient({
-      type: "socket",
-      socket,
-      initializationOptions: this.settings.languageClientConfig.initializationOptions
-    });
-    acodeLanguageClient.registerService(this.name_language_type, JavaClient);
-
-
-    let handler = () => {
-      JavaClient.connection.onNotification("window/showMessage", ({ params }) => {
-        console.log("ShowMessage: " + params.message)
-      });
-      JavaClient.connection.onNotification("workspace/configuration", params => {
-        const deb = "Initilizing Progress " + this.languageserver
-        console.log(deb + `${params.message}`)
-
-      });
-      JavaClient.connection.onRequest("window/workDoneProgress/create", params => {
-        const deb = "Initializing WorkDone Progress " + this.languageserver
-        console.log(deb + `${params.message}`)
-
-      });
+        return value;
     }
-    socket.addEventListener("open", () => {
-      if (JavaClient.isInitialized) handler();
-      else JavaClient.requestsQueue.push(() => handler());
-      console.log("OPEN CONNECTION")
-    });
 
-    acode.registerFormatter(plugin.name, [this.name_language_type], () =>
-      acodeLanguageClient.format(),
-    );
+    get settingsMenuLayout() {
+        return {
+            list: [
+                {
+                    index: 0,
+                    key: "serverPath",
+                    promptType: "text",
+                    prompt: "Change the serverPath before running.",
+                    text: "JDTLS Executable File Path",
+                    value: this.settings.serverPath,
+                },
+                {
+                    index: 1,
+                    key: "arguments",
+                    promptType: "text",
+                    info: "For multiple arguments, please use comma ','<br>Example: --stdio, -v, -vv",
+                    prompt: "Argument Of Language Server",
+                    text: "JDTLS Argument",
+                    value: this.settings.arguments.join(", "),
+                },
+            ],
 
-  }
-
-  async destroy() {
-    if (AppSettings.value[plugin.id]) {
-      delete AppSettings.value[plugin.id];
-      AppSettings.update();
+            cb: (key, value) => {
+                switch (key) {
+                    case "arguments":
+                        value = value
+                            ? value.split(",").map((item) => item.trim())
+                            : [];
+                        break;
+                }
+                AppSettings.value[plugin.id][key] = value;
+                AppSettings.update();
+            },
+        };
     }
-  }
+
+    get defaultSettings() {
+        return {
+            serverPath: this.languageserver,
+            arguments: this.standart_args,
+            languageClientConfig: this.initializeOptions,
+        };
+    }
+
+    async setupLanguageClient(acodeLanguageClient) {
+        let socket = acodeLanguageClient.getSocketForCommand(
+            this.settings.serverPath,
+            this.settings.arguments,
+        );
+
+        let JavaClient = new acodeLanguageClient.LanguageClient({
+            type: "socket",
+            socket,
+            initializationOptions:
+                this.settings.languageClientConfig.initializationOptions,
+        });
+        acodeLanguageClient.registerService(
+            this.name_language_type,
+            JavaClient,
+        );
+
+        // let handler = () => {
+        //     JavaClient.connection.onNotification(
+        //         "window/showMessage",
+        //         ({ params }) => {
+        //             console.log("ShowMessage: " + params.message);
+        //         },
+        //     );
+        //     JavaClient.connection.onNotification(
+        //         "workspace/configuration",
+        //         (params) => {
+        //             const deb = "Initilizing Progress " + this.languageserver;
+        //             console.log(deb + `${params.message}`);
+        //         },
+        //     );
+        //     JavaClient.connection.onRequest(
+        //         "window/workDoneProgress/create",
+        //         (params) => {
+        //             const deb =
+        //                 "Initializing WorkDone Progress " + this.languageserver;
+        //             console.log(deb + `${params.message}`);
+        //         },
+        //     );
+        // };
+        // socket.addEventListener("open", () => {
+        //     if (JavaClient.isInitialized) handler();
+        //     else JavaClient.requestsQueue.push(() => handler());
+        //     console.log("OPEN CONNECTION");
+        // });
+
+        acode.registerFormatter(plugin.id, ["java"], async () => {
+            const test = () => {
+                const javaCode = editorManager.editor.session.getValue();
+
+                return import("prettier/standalone")
+                    .then((prettier) => {
+                        return import("prettier-plugin-java").then(
+                            (parserJava) => {
+                                console.log(
+                                    "Prettier and Java plugin loaded successfully.",
+                                );
+                                return prettier.format(javaCode, {
+                                    parser: "java",
+                                    plugins: [parserJava.default],
+                                    tabWidth: 4,
+                                    useTabs: false,
+                                    printWidth: 120,
+                                });
+                            },
+                        );
+                    })
+                    .then((formatted) => {
+                        console.log("Formatted Code:", formatted);
+                        return formatted;
+                    })
+                    .catch((error) => {
+                        console.error(
+                            "Error formatting Java code:",
+                            error.message || error,
+                        );
+
+                        if (error.name === "SyntaxError") {
+                            console.warn(
+                                "The provided code contains syntax errors and cannot be formatted.",
+                            );
+                        }
+                        return editorManager.editor.session.getValue();
+                    });
+            };
+
+            test()
+                .then((formattedCode) => {
+                    editorManager.editor.session.setValue(formattedCode);
+                })
+                .catch((error) => {
+                    console.error(
+                        "Error setting formatted code:",
+                        error.message || error,
+                    );
+                });
+        });
+    }
+
+    async destroy() {
+        if (AppSettings.value[plugin.id]) {
+            delete AppSettings.value[plugin.id];
+            AppSettings.update();
+        }
+        acode.unregisterFormatter(plugin.id);
+    }
 }
 
 if (window.acode) {
-  const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(
-    plugin.id,
-    async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-      if (!baseUrl.endsWith("/")) {
-        baseUrl += "/";
-      }
-      acodePlugin.baseUrl = baseUrl;
-      await acodePlugin.init($page, cacheFile, cacheFileUrl);
-    },
-    acodePlugin.settingsMenuLayout,
-  );
+    const acodePlugin = new AcodePlugin();
+    acode.setPluginInit(
+        plugin.id,
+        async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+            if (!baseUrl.endsWith("/")) {
+                baseUrl += "/";
+            }
+            acodePlugin.baseUrl = baseUrl;
+            await acodePlugin.init($page, cacheFile, cacheFileUrl);
+        },
+        acodePlugin.settingsMenuLayout,
+    );
 
-  acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
-  });
-
+    acode.setPluginUnmount(plugin.id, () => {
+        acodePlugin.destroy();
+    });
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,95 @@
-const { exec } = require('child_process');
-const path = require('path');
+const { exec } = require("child_process");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = (env, options) => {
-  const { mode = 'development' } = options;
-  const rules = [
-    {
-      test: /\.m?js$/,
-      use: [
-        'html-tag-js/jsx/tag-loader.js',
+    const { mode = "production" } = options;
+    const rules = [
         {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
+            test: /\.m?js$/,
+            use: [
+                "html-tag-js/jsx/tag-loader.js",
+                {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            ],
         },
-      ],
-    },
-  ];
+        {
+            test: /\.ts$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        },
+    ];
 
-  const main = {
-    mode,
-    entry: {
-      main: './src/main.js',
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-    },
-    module: {
-      rules,
-    },
-    plugins: [
-      {
-        apply: (compiler) => {
-          compiler.hooks.afterDone.tap('pack-zip', () => {
-            // run pack-zip.js
-            exec('node .vscode/pack-zip.js', (err, stdout, stderr) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-              console.log(stdout);
-            });
-          });
-        }
-      }
-    ],
-  };
+    const main = {
+        mode,
+        entry: {
+            main: "./src/main.js",
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "[name].js",
+            chunkFilename: "[name].js",
+        },
+        module: {
+            rules,
+        },
+        node: {
+            __filename: true,
+            __dirname: true,
+            global: true,
+        },
+        resolve: {
+            extensions: [".ts", ".js"],
+            fallback: {
+                path: require.resolve("path-browserify"),
+                util: require.resolve("util/"),
+                assert: require.resolve("assert/"),
+                os: false,
+                fs: false,
+                module: false,
+                v8: false,
+                path: false,
+                url: false,
+            },
+        },
+        plugins: [
+            new NodePolyfillPlugin(),
+            {
+                apply: (compiler) => {
+                    compiler.hooks.afterDone.tap("pack-zip", () => {
+                        // run pack-zip.js
+                        exec(
+                            "node .vscode/pack-zip.js",
+                            (err, stdout, stderr) => {
+                                if (err) {
+                                    console.error(err);
+                                    return;
+                                }
+                                console.log(stdout);
+                            },
+                        );
+                    });
+                },
+            },
+        ],
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    // Use multi-process parallel running to improve the build speed
+                    // Default number of concurrent runs: os.cpus().length - 1
+                    parallel: true,
+                    // Enable file caching
+                    // cache: true,
+                    // minify: true
+                }),
+            ],
+        },
+    };
 
-  return [main];
-}
+    return [main];
+};
